5262dcff678179578f9b6d9e89c9e52341f99029
Takahiro Kaneko <tkaneko188@gmail.com>
2026-03-25 23:12:15 +0900

    fix: adjust sqrt precision

--- lib/calc/functions/sqrt.rb
+++ lib/calc/functions/sqrt.rb
@@ -5,7 +5,9 @@ module Calc
     module Sqrt
       def self.register(builtins)
         builtins.register("sqrt", min_arity: 1, max_arity: 1) do |args|
-          args.first.sqrt(32)
+          value = args.first
+          precision = [value.precision + 10, 16].max
+          value.sqrt(precision)
         end
       end
     end
